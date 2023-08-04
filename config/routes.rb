d5cc14ba18be937d9fe620a7021640f0d99c0e79
cyaomasa <cyao.masa.0924@gmail.com>
2023-08-04 07:22:55 +0000

    [Add]controllers/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   devise_for :admins
+  
+  root to:'public/homes#top'
+  
+  namespace :admin do
+    resources :users, only: [:index, :show, :destroy]
+    resources :post_records, only: [:index, :show, :destroy] do
+      resources :comments, only: [:create, :destroy]
+    end
+    resources :categories, only: [:index, :create, :edit, :update]
+    
+  end
+
+  scope module: :public do
+    resources :users, only: [:index, :edit, :update]
+    resources :post_records do
+      resource :favorites, only: [:index, :create, :destroy]
+      resources :comments, only: [:create, :destroy]
+    end
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/public/users_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class Public::UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get public_users_index_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get public_users_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get public_users_update_url
+    assert_response :success
+  end
+end

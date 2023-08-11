565c988b9ed66c3fac5f39cf208c87007fef0bc7
cyaomasa <cyao.masa.0924@gmail.com>
2023-08-11 07:33:44 +0000

    [Add]admin_post_record

--- app/controllers/admin/post_records_controller.rb
+++ app/controllers/admin/post_records_controller.rb
@@ -1,8 +1,11 @@
 class Admin::PostRecordsController < ApplicationController
+  
   def index
+    @post_records = PostRecord.order(created_at: :desc)
   end
 
   def show
+    @post_record = PostRecord.find(params[:id])
   end
 
   def destroy

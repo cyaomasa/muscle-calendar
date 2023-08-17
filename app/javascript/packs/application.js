5fc8cdf1b3517b79c74a028160198e56145eb54f
cyaomasa <cyao.masa.0924@gmail.com>
2023-08-17 07:27:52 +0000

    [Add]top

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,11 @@ import "../stylesheets/simple_calendar";
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+/*global $*/
+$(function() {
+  $('.slider').slick({
+    prevArrow: '<i class="fas fa-arrow-alt-circle-left"></i>',
+    nextArrow: '<i class="fas fa-arrow-alt-circle-right"></i>',
+  })
+});

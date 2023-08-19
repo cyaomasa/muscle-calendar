c00fa79d2221f4b087112dcb83dad5ea6f59f68d
cyaomasa <cyao.masa.0924@gmail.com>
2023-08-19 02:29:34 +0000

    [Update]layouts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,5 +23,6 @@ $(function() {
   $('.slider').slick({
     prevArrow: '<i class="fas fa-arrow-alt-circle-left"></i>',
     nextArrow: '<i class="fas fa-arrow-alt-circle-right"></i>',
+    dots:true
   })
 });
